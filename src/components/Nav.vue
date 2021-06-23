8db725f014e1ce2db33546b19708858c71495e89
Nevermorey98 <Nevermorey98@gmail.com>
2021-06-23 22:11:00 +0800

    创建 Icon 组件

--- src/components/Nav.vue
+++ src/components/Nav.vue
@@ -1,17 +1,31 @@
 <template>
   <div class="nav">
-    <router-link to="/money">记账</router-link>
+    <router-link to="/money">
+      <Icon name="account"/>
+      记账
+    </router-link>
     |
-    <router-link to="/labels">标签</router-link>
+    <router-link to="/labels">
+      <Icon name="labels"/>
+      标签
+    </router-link>
     |
-    <router-link to="/statistics">统计</router-link>
+    <router-link to="/statistics">
+      <Icon name="statistics-circle"/>
+      统计
+    </router-link>
   </div>
 </template>
 
 <script lang="ts">
+
+import x from '@/assets/icons/account.svg';
+
+console.log(x);
+
 export default {
-  name: "Nav"
-}
+  name: 'Nav'
+};
 </script>
 
 <style lang="scss" scoped>
--- src/router/index.ts
+++ src/router/index.ts
@@ -1,11 +1,11 @@
-import Vue from 'vue'
-import VueRouter, {RouteConfig} from 'vue-router'
-import Money from '@/views/Money.vue'
-import Labels from '@/views/Labels.vue'
-import Statistics from '@/views/Statistics.vue'
-import NotFound from '@/views/NotFound.vue'
+import Vue from 'vue';
+import VueRouter, {RouteConfig} from 'vue-router';
+import Money from '@/views/Money.vue';
+import Labels from '@/views/Labels.vue';
+import Statistics from '@/views/Statistics.vue';
+import NotFound from '@/views/NotFound.vue';
 
-Vue.use(VueRouter)
+Vue.use(VueRouter);
 
 const routes: Array<RouteConfig> = [
   {
@@ -30,10 +30,10 @@ const routes: Array<RouteConfig> = [
     component: NotFound
   }
 
-]
+];
 
 const router = new VueRouter({
   routes
-})
+});
 
-export default router
+export default router;
