0d6b753e628aad26eecb6084bcee6de1ba6e4783
Nevermorey98 <Nevermorey98@gmail.com>
2021-06-23 20:10:31 +0800

    添加 404 页面

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,9 +1,9 @@
 import Vue from 'vue'
-import VueRouter, { RouteConfig } from 'vue-router'
-import Home from '../views/Home.vue'
+import VueRouter, {RouteConfig} from 'vue-router'
 import Money from '@/views/Money.vue'
 import Labels from '@/views/Labels.vue'
 import Statistics from '@/views/Statistics.vue'
+import NotFound from '@/views/NotFound.vue'
 
 Vue.use(VueRouter)
 
@@ -36,7 +36,12 @@ const routes: Array<RouteConfig> = [
   {
     path: '/statistics',
     component: Statistics
+  },
+  {
+    path: '*',
+    component: NotFound
   }
+
 ]
 
 const router = new VueRouter({
