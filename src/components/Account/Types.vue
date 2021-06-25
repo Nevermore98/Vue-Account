c2a3b19e18fb57439158812e5f3801007064f710
Nevermorey98 <Nevermorey98@gmail.com>
2021-06-25 17:25:49 +0800

    完成 Types 组件

--- src/components/Account/Types.vue
+++ src/components/Account/Types.vue
@@ -9,18 +9,11 @@
 
 <script lang="ts">
 import Vue from 'vue';
-import {Component, Prop} from 'vue-property-decorator';
+import {Component} from 'vue-property-decorator';
 
-@Component({
-  props: {
-    propMessage: String
-  }
-})
+@Component
 export default class Types extends Vue {
   type = '-';
-  @Prop(String) xxx: string | undefined;
-  // 前面 String 告诉 Vue 运行时类型，后面 string 告诉 TS 编译时检查类型
-  @Prop(Number) yyy: number | undefined;
 
   selectType(type: string) {
     if (type !== '-' && type !== '+') {
@@ -29,42 +22,8 @@ export default class Types extends Vue {
     this.type = type;
   }
 
-  mounted() {
-    if (this.yyy !== undefined) {
-      console.log(this.yyy.toString());
-    } else {
-      console.log('yyy 未定义');
-    }
-    console.log(this.xxx);
-    console.log(typeof this.xxx);
-    console.log(this.yyy);
-    console.log(typeof this.yyy);
-  }
 }
 </script>
-
-<!--<script lang="js">-->
-<!--export default {-->
-<!--  name: 'Types',-->
-<!--  props: ['xxx'],-->
-<!--  data() {-->
-<!--    return {-->
-<!--      type: '-' // '-' 表示支出 '+' 表示收入-->
-<!--    };-->
-<!--  },-->
-<!--  mounted() {-->
-<!--    console.log(this.xxx)-->
-<!--  },-->
-<!--  methods: {-->
-<!--    selectType(type) {-->
-<!--      if (type !== '-' && type !== '+') {-->
-<!--        throw new Error('类型错误');-->
-<!--      }-->
-<!--      this.type = type;-->
-<!--    }-->
-<!--  }-->
-<!--};-->
-<!--</script>-->
 
 <style lang="scss" scoped>
 .types {
