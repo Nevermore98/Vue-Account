64902a0bebea5575f6d9ba36c414b7e40a474a2b
Nevermorey98 <Nevermorey98@gmail.com>
2021-06-25 17:23:59 +0800

    使用 TS 完成组件

--- src/components/Account/Types.vue
+++ src/components/Account/Types.vue
@@ -9,17 +9,36 @@
 
 <script lang="ts">
 import Vue from 'vue';
-import {Component} from 'vue-property-decorator';
+import {Component, Prop} from 'vue-property-decorator';
 
-@Component
+@Component({
+  props: {
+    propMessage: String
+  }
+})
 export default class Types extends Vue {
   type = '-';
+  @Prop(String) xxx: string | undefined;
+  // 前面 String 告诉 Vue 运行时类型，后面 string 告诉 TS 编译时检查类型
+  @Prop(Number) yyy: number | undefined;
 
   selectType(type: string) {
     if (type !== '-' && type !== '+') {
       throw new Error('类型错误');
     }
     this.type = type;
+  }
+
+  mounted() {
+    if (this.yyy !== undefined) {
+      console.log(this.yyy.toString());
+    } else {
+      console.log('yyy 未定义');
+    }
+    console.log(this.xxx);
+    console.log(typeof this.xxx);
+    console.log(this.yyy);
+    console.log(typeof this.yyy);
   }
 }
 </script>
