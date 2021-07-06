2d2802d1b01ff56edcc14f9d987ce7cf9bf37669
Nevermorey98 <Nevermorey98@gmail.com>
2021-07-06 14:51:36 +0800

    实现标签页的标签查看、新建

--- src/views/Account.vue
+++ src/views/Account.vue
@@ -16,16 +16,18 @@ import Types from '@/components/Account/Types.vue';
 import Remark from '@/components/Account/Remark.vue';
 import Tags from '@/components/Account/Tags.vue';
 import {Component, Watch} from 'vue-property-decorator';
-import model from '@/model';
+import recordListModel from '@/models/recordListModel';
+import tagListModel from '@/models/tagListModel';
 
-const recordList = model.fetch();
+const recordList = recordListModel.fetch();
+const tagList = tagListModel.fetch;
 
 @Component({
   components: {Tags, Remark, Types, NumberPad},
 })
 export default class Account extends Vue {
-  tags = ['衣', '食', '住', '行'];
-  // 初始值
+
+  tags = tagList;
   record: RecordItem = {
     tags: [], remarks: '', type: '-', amount: 0, createdAt: new Date(0)
   };
@@ -45,7 +47,7 @@ export default class Account extends Vue {
   }
 
   saveRecord() {
-    const tempRecord: RecordItem = model.clone(this.record);
+    const tempRecord: RecordItem = recordListModel.clone(this.record);
     tempRecord.createdAt = new Date();
     this.recordList.push(tempRecord);
     console.log(this.recordList);
@@ -54,7 +56,7 @@ export default class Account extends Vue {
 
   @Watch('recordList')
   onRecordListChange() {
-    model.save(this.recordList);
+    recordListModel.save(this.recordList);
   }
 }
 
